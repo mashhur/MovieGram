64659952b28af3d985419f1656eebb9db9ccdefc
mashhur <mashhur.sattorov@gmail.com>
2017-04-04 16:25:26 +0900

    Thymeleaf embedded (Test: localhost:8080/greeting). We can now choose either way (Thymeleaf or JSTL)

--- src/main/java/org/learnteachcode/seoul/api/MoviesApiController.java
+++ src/main/java/org/learnteachcode/seoul/api/MoviesApiController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.util.UUID;
+
 /**
  * Created by mashhur on 3/22/17.
  */
@@ -15,5 +18,22 @@ public class MoviesApiController {
     @ResponseBody
     String home() {
         return "Welcome to MovieGram!";
+    }
+
+    @RequestMapping("/api/getUniqueKey")
+    @ResponseBody
+    String generateKey() {
+        String strRet = "";
+        try {
+            // generate globally unique key (considering host name)
+            String host = InetAddress.getLocalHost().getHostName() + UUID.randomUUID().toString();
+            String sKey = UUID.nameUUIDFromBytes(host.getBytes()).toString();
+            strRet = host + "-"+ sKey;
+        } catch (Exception ex){
+            // in case of exception, generate an unique value (guaranteed only in one VM)
+            strRet = UUID.randomUUID().toString();
+        }
+
+        return strRet;
     }
 }
--- src/main/java/org/learnteachcode/seoul/ui/Controller/MovieController.java
+++ src/main/java/org/learnteachcode/seoul/ui/Controller/MovieController.java
@@ -2,7 +2,9 @@ package org.learnteachcode.seoul.ui.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -21,5 +23,11 @@ public class MovieController {
     public String welcome(Map<String, Object> model) {
         model.put("message", this.message);
         return "welcome";
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
