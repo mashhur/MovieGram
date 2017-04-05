fd231e92e2fe905c77890209b9645cda7ceee166
mashhur <mashhur.sattorov@gmail.com>
2017-04-05 13:46:32 +0900

    JSP deleted, now it is pure Thymeleaf project.

--- src/main/java/org/learnteachcode/seoul/api/moviedb/MovieDBApiController.java
+++ src/main/java/org/learnteachcode/seoul/api/moviedb/MovieDBApiController.java
@@ -2,6 +2,7 @@ package org.learnteachcode.seoul.api.moviedb;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,15 +14,11 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MovieDBApiController {
 
-    /* The movie DB
-    // Please visit http://www.omdbapi.com/ for more info
-    */
-    // base API url of movie database
-    private final String THEMOVIE_API_URL = "https://api.themoviedb.org/3/"; // 3rd version
+    @Value("${themoviedb.api:}")
+    private String api_url = "api";
 
-    // a key
-    private final String THEMOVIE_API_KEY = "api_key=6ee9ac55f15c253301af94c390c30c23";
-    private final String THEMOVIE_API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZWU5YWM1NWYxNWMyNTMzMDFhZjk0YzM5MGMzMGMyMyIsInN1YiI6IjU4ZTM1Yjg1OTI1MTQxMjdlYzAxZGMyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9xJ0imNR0tm0_B9GQVYRtD9KeLT_u7arckN-v06iMls";
+    @Value("${themoviedb.key:}")
+    private String api_key = "api_key";
 
     // Logger
     private static final Logger LOGGER = LoggerFactory.getLogger(MovieDBApiController.class);
@@ -36,7 +33,7 @@ public class MovieDBApiController {
     String discover(@RequestParam(value = "type", required = false) String type) {
         LOGGER.debug("Request with movie type -> ", type);
 
-        final String uri = THEMOVIE_API_URL + "discover/movie?" + THEMOVIE_API_KEY;
+        final String uri = api_url + "discover/movie?" + api_key;
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri , String.class);
         return result;
